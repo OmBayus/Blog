4539ab6f92a5e5cb3a717d0ef8fe76cf67bfd86e
OmBayus <omerbayramcavus@gmail.com>
2021-04-06 11:59:00 +0300

    edit and delete dialog screen in Blog Panel Posts

--- client/src/Blog/db.js
+++ client/src/Blog/db.js
@@ -3,7 +3,7 @@ const db = [
             id:1,
             title:"Title 1",
             topic:"Travel",
-            imgUrl:"/resim.webp",
+            imgUrl:"/default.webp",
             date: new Date("October 1, 2020"),
             details:[
                   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
@@ -37,7 +37,7 @@ const db = [
             id:2,
             title:"Title 2",
             topic:"Fun",
-            imgUrl:"/resim.webp",
+            imgUrl:"/default.webp",
             date: new Date("February 5, 2020"),
             details:[
                   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
@@ -71,7 +71,7 @@ const db = [
             id:3,
             title:"Title 3",
             topic:"Travel",
-            imgUrl:"/resim.webp",
+            imgUrl:"/default.webp",
             date: new Date("October 7, 2020"),
             details:[
                   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
@@ -105,7 +105,7 @@ const db = [
             id:4,
             title:"Title 4",
             topic:"Travel",
-            imgUrl:"/resim.webp",
+            imgUrl:"/default.webp",
             date: new Date("October 5, 2020"),
             details:[
                   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
--- client/src/BlogPanel/components/Posts.jsx
+++ client/src/BlogPanel/components/Posts.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React,{useState} from 'react';
+
+//dummy data
+import {db} from "../../Blog/db"
 
 //Material Ui
 import { makeStyles } from '@material-ui/core/styles';
@@ -9,12 +12,28 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Dialog from '@material-ui/core/Dialog';
+import TextField from '@material-ui/core/TextField';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import MuiAlert from '@material-ui/lab/Alert';
 
 //Bootstrap
 import {Container,Row,Col} from "react-bootstrap"
 
 //style
 import style from "../styles/Posts.module.css"
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+      return <Slide direction="up" ref={ref} {...props} />;
+});
+
+function Alert(props) {
+      return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const useStyles = makeStyles({
       root: {
@@ -27,35 +46,117 @@ const useStyles = makeStyles({
     });
 
 const Posts = ()=>{
+
       const classes = useStyles();
+
+      const [popup,setPopup] = useState({
+            del:false,
+            edit:{
+                  isOpen:false,
+                  info:{
+                        title:"",
+                        topic:"",
+                        imgUrl:"",
+                        details:["1","2"]
+                  }
+            }
+      })
+      
+
+      const handlePost = (e)=>{
+            const {name,value} = e.target
+
+            if(name === "title"){
+                  setPopup(prevValue=>(
+                        {...prevValue,edit:{
+                              isOpen:true,
+                              info:{
+                                    ...prevValue.edit.info,
+                                    title:value
+                              }
+                        }}
+                  ))
+            }
+            else if (name==="topic"){
+                  setPopup(prevValue=>(
+                        {...prevValue,edit:{
+                              isOpen:true,
+                              info:{
+                                    ...prevValue.edit.info,
+                                    topic:value
+                              }
+                        }}
+                  ))
+            }
+      }
+
+      const handleDetails = (index,value)=>{
+            const temp = popup.edit.info.details
+            temp[index] = value 
+
+            setPopup(prevValue=>(
+                  {...prevValue,edit:{
+                        isOpen:true,
+                        info:{
+                              ...prevValue.edit.info,
+                              details:temp
+                        }
+                  }}
+            ))
+      }
+
+
+
+      const deletePost = ()=>{
+            console.log(popup)
+      }
+
+      const editPost = ()=>{
+            console.log(popup)
+      }
+
+
+
+
       return(
-      <Container fluid className={style.Posts}>
+      <div><Container fluid className={style.Posts}>
             <Row>
             {
-                  [1,2,3,4,5].map(item=>(
-                  <Col md={3} key={item}>
+                  db.map(item=>(
+                  <Col md={3} key={item.id}>
                         <Card className={classes.root}>
                               <CardActionArea>
                                     <CardMedia
                                     className={classes.media}
-                                    image="http://localhost:4000/uploads/default.webp"
+                                    image={"http://localhost:4000/uploads"+item.imgUrl}
                                     title="Contemplative Reptile"
                                     />
                                     <CardContent>
                                     <Typography gutterBottom variant="h5" component="h2">
-                                    Title
+                                    {item.title}
                                     </Typography>
                                     <Typography variant="body2" color="textSecondary" component="p">
-                                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                    across all continents except Antarctica
+                                    {item.details[0].substr(0, 186)}
                                     </Typography>
                                     </CardContent>
                               </CardActionArea>
                               <CardActions>
-                                    <Button size="small" color="primary">
+                                    <Button size="small" color="primary" onClick={()=>{
+                                          setPopup(prevValue=>({...prevValue,edit:{
+                                                isOpen:true,
+                                                info:{
+                                                      title:item.title,
+                                                      topic:item.topic,
+                                                      imgUrl:item.imgUrl,
+                                                      details:item.details
+                                                }
+                                          }}))
+                                    }}>
                                     Edit
                                     </Button>
-                                    <Button size="small" color="secondary">
+                                    <Button size="small" color="secondary" onClick={()=>setPopup(prevValue=>({
+                                          ...prevValue,del:true
+                                    }))}>
                                     Delete
                                     </Button>
                               </CardActions>
@@ -64,7 +165,118 @@ const Posts = ()=>{
                   ))   
             }
             </Row>
-      </Container>)
+      </Container>
+      {/* Delete Post */}
+      <Dialog
+                  open={popup.del}
+                  TransitionComponent={Transition}
+                  keepMounted
+                  onClose={()=>setPopup(prevValue=>({
+                        ...prevValue,del:false
+                  }))}
+                  aria-labelledby="alert-dialog-slide-title"
+                  aria-describedby="alert-dialog-slide-description"
+                  >
+                  <DialogTitle id="alert-dialog-slide-title">{"Delete Post"}</DialogTitle>
+                  <DialogContent>
+                  <DialogContentText id="alert-dialog-slide-description">
+                  The post will be deleted! <br/>Are You Sure?
+                  </DialogContentText>
+                  </DialogContent>
+                  <DialogActions>
+                  <Button onClick={()=>setPopup(prevValue=>({
+                        ...prevValue,del:false
+                  }))} color="primary">
+                        Cancel
+                  </Button>
+                  <Button onClick={deletePost} color="primary">
+                        Yes
+                  </Button>
+                  </DialogActions>
+      </Dialog>
+      {/* Edit Post */}
+      <Dialog 
+            open={popup.edit.isOpen} 
+            TransitionComponent={Transition} 
+            onClose={()=>setPopup(prevValue=>({
+                        ...prevValue,
+                        edit:{
+                              isOpen:false,
+                              info:{
+                                    title:"",
+                                    topic:"",
+                                    imgUrl:"",
+                                    details:[]
+                              }
+                        }
+                  }))} 
+                  aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Edit Post</DialogTitle>
+            <DialogContent>
+            <DialogContentText>
+                  Update the post
+            </DialogContentText>
+                  <TextField
+                        autoFocus
+                        margin="dense"
+                        value={popup.edit.info.title}
+                        type="text"
+                        name="title"
+                        label="Title"
+                        onChange={handlePost}
+                        fullWidth
+                  />
+                  <TextField
+                        autoFocus
+                        margin="dense"
+                        name="topic"
+                        label="Topic"
+                        value={popup.edit.info.topic}
+                        onChange={handlePost}
+                        type="text"
+                        fullWidth
+                  />
+                  {
+                        popup.edit.info.details.map((detail,index)=>{
+                              return(
+                                    <TextField
+                                          key={index}
+                                          autoFocus
+                                          margin="dense"
+                                          name="Detail"
+                                          label={"Details " + (index+1)}
+                                          value={detail}
+                                          onChange={(e)=>handleDetails(index,e.target.value)}
+                                          type="text"
+                                          fullWidth
+                                    />
+                              )
+                        })
+                  }
+                  
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={()=>setPopup(prevValue=>({
+                        ...prevValue,
+                        edit:{
+                              isOpen:false,
+                              info:{
+                                    title:"",
+                                    topic:"",
+                                    imgUrl:"",
+                                    details:[]
+                              }
+                        }
+                  }))} color="primary">
+                  İptal
+            </Button>
+            <Button onClick={editPost} color="primary">
+                  Gönder
+            </Button>
+            </DialogActions>
+      </Dialog>
+      </div>
+      )
 }
 
 
