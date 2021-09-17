74929f63323afb738e45895215026c51393c2927
Omer Bayramcavus <47576905+OmBayus@users.noreply.github.com>
2021-09-18 00:26:42 +0300

    Update config.js

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT || 4000
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/blog"
 
 module.exports = {
   MONGODB_URI,
